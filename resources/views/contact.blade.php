121e2c191ecd81c840f4d103d2d4cb712d10e49f
vmichel4810 <v.michel@it-students.fr>
2021-07-27 10:59:44 +0200

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.base')
+
+@section('title', 'contact')
+@section('css', 'contact')
+
+@section('content')
+<h1>contact</h1>
+@endsection
+
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.base')
+
+@section('title', 'Accueil')
+
+@section('content')
+<h1>Accueil</h1>
+@endsection
+
